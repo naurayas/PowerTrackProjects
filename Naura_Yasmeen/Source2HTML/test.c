fc8b9b7f55003018c65066a7a47968900b89a5f7
Naura Yasmeen U <96697831+naurayas@users.noreply.github.com>
2026-07-09 10:41:00 +0530

    Add files via upload

---
+++ Naura_Yasmeen/Source2HTML/test.c
@@ -0,0 +1,12 @@
+/* this is multi-line comment */
+#include <stdio.h>
+#define MAX 20
+#ifdef DEBUG
+#endif
+int main()  // this is single line comment
+{
+    char a = 'A';
+    char str[] = "hello";
+    int x = 0x10;
+    return 0;
+}
